fb1167a1edf84cdfc5f9723542b020c2ea6ae4d1
bluejamesbond <MathewKurian@utexas.edu>
2016-12-27 15:12:13 -0600

    Init commit

---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var spawn = require('child_process').spawn;
+
+program
+  .version('0.0.1')
+  .option('-c, --only-children', 'Run children only')
+  .option('-p, --parallel', 'Run each child parallel')
+  .parse(process.argv);
+
+function exec(command) {
+  if (!~process.platform.indexOf('win')) {
+    return spawn('cmd', ['/s', '/c', command], { stdio: ['ignore', process.stdout, process.stderr] })
+  } else {
+    return spawn('/bin/sh', ['-c', command], { stdio: ['ignore', process.stdout, process.stderr] })
+  }
+}
+
+var command = program.args.map(function (a) {
+  return decodeURIComponent(a.split(' ').length > 1 ? a : a);
+}).join(' ');
+
+var child = exec('git submodule foreach \'./' + __dirname + ' ' + encodeURIComponent(command) + '\' || :');
+
+child.on('exit', function (code) {
+  if (code !== 0 || process.onlyChildren) {
+    return process.exit(code)
+  }
+
+  exec(command);
+});
